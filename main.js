53be4f3fb0ecbf2932e7322e1bb2c01543dfbd2f
Javier Saez <banbleac@gmail.com>
2022-12-19 17:48:04 +0100

    Segundo Commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import { BreakingbadApp } from './src/breakingbad/breakingbad-app';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -10,14 +10,14 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1 id="app-title">Hello Http-App!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+   
   </div>
-`
+`;
+const element = document.querySelector('.card');
+BreakingbadApp( element );
 
-setupCounter(document.querySelector('#counter'))
+
---
+++ src/breakingbad/breakingbad-app.js
@@ -0,0 +1,56 @@
+
+/**
+ * @returns { Promise<Object> } quote information
+ */
+
+// Por que devuelve una promesa puedo poner async
+const fetchQuote = async() => {
+
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes/20');
+    const data = await res.json();
+    console.log(data[0]);
+    return data[0];
+}
+
+
+
+/**
+ * 
+ * @param { HTMLDivElement } element 
+ */
+
+export const BreakingbadApp = async( element ) => {
+
+    document.querySelector('#app-title').innerText = 'Breakingbad App';
+    element.innerHTML = 'Loading...';
+    const quote = await fetchQuote();
+
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = ( data ) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren( authorLabel, quoteLabel,  nextQuoteButton);
+    }
+
+    // añadir listener
+    nextQuoteButton.addEventListener('click', async() => {
+        element.innerHTML = 'Loading...';
+        nextQuoteButton.disabled = true;
+        
+        
+        
+        const quote = await fetchQuote();
+        renderQuote( quote );
+        nextQuoteButton.disabled = false;
+
+    })
+
+    fetchQuote()
+        .then( renderQuote );
+    
+
+}
